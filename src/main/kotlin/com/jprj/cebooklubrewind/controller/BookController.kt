5aa9ee589b6c5a76d52a0e48ac75b220e7d256a8
John Paul Jayme <johnj@codev.com>
2025-05-28 12:24:38 +0800

    Last push

--- src/main/kotlin/com/jprj/cebooklubrewind/controller/BookController.kt
+++ src/main/kotlin/com/jprj/cebooklubrewind/controller/BookController.kt
@@ -1,9 +1,14 @@
 package com.jprj.cebooklubrewind.controller
 
 import com.jprj.cebooklubrewind.service.BlogService
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxReswap
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRetarget
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxSwapType
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 
 @Controller
 class BookController(
@@ -11,10 +16,29 @@ class BookController(
 ) {
 
     @GetMapping("/")
-    fun index(model: Model): String {
+    fun index(model: Model,
+              @RequestHeader(value = "HX-Request", defaultValue = "false") isHxRequest: Boolean): String {
 
         model.addAttribute("currentBook",  blogService.getCurrentRead())
-        println(model)
-        return "index"
+        return if (isHxRequest) "fragments/fragments :: currentRead" else "index"
+//        return "fragments/fragments :: pastReads"
+    }
+
+
+    @HxRequest
+    @HxReswap(value = HxSwapType.INNER_HTML)
+    @HxRetarget(value = "#main-content")
+    @GetMapping("/pastReads")
+    fun pastReads(model: Model): String {
+        model.addAttribute("books",  blogService.list())
+        return "fragments/fragments :: pastReads"
+    }
+
+    @HxRequest
+    @HxReswap(value = HxSwapType.INNER_HTML)
+    @HxRetarget(value = "#main-content")
+    @GetMapping("/aboutUs")
+    fun aboutUs(model: Model): String {
+        return "fragments/fragments :: aboutUs"
     }
 }
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("nav--overlay").classList.add("active");
+
+}
+
+function closeNav(){
+    document.getElementById("nav--overlay").classList.remove("active");
+}
